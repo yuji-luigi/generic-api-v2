c96bea3794a6f33ae6e7a8a9e4cadeb5e4c7fde0
Yuji Sato <--replace-all>
2023-03-05 16:31:35 +0100

    RequestCustomInterface lo longer be used

--- src/types/express-custom.d.ts
+++ src/types/express-custom.d.ts
@@ -9,16 +9,44 @@ interface TypedRequestBody<T, U> extends Request {
 type QueryReturnType = string | number | boolean | undefined;
 
 
-interface RequestCustom extends Request {
-  logIn: (userId: string | IUser) => Promise<void>;
-  user?: IUser;
-  params: { 
-    [key: string]: string
-    idMongoose?: string;
-    id?: string;
-    userId?: string
-   };
-  query?: { 
+// interface RequestCustom extends RequestHandlerParams<ParamsDictionary, any, any, ParsedQs, Record<string, any>> {
+//   logIn: (userId: string | IUser) => Promise<void>;
+//   user?: IUser;
+//   params: { 
+//     [key: string]: string
+//     idMongoose?: string;
+//     id?: string;
+//     userId?: string
+//    };
+//   query?: { 
+//     spaces?: QueryReturnType;
+//     users?: QueryReturnType;
+//     proposals?: QueryReturnType;
+//     funds?: QueryReturnType;
+//     fundRules?: QueryReturnType;
+//     instances?: QueryReturnType;
+//     threads?: QueryReturnType;
+//     comments?: QueryReturnType;
+//     tags?: QueryReturnType;
+//     bookmarks?: QueryReturnType;
+//     wallets?: QueryReturnType;
+//     userSettings?: QueryReturnType;
+//     space?: QueryReturnType;
+//     user?: QueryReturnType;
+//     proposal?: QueryReturnType;
+//     fund?: QueryReturnType;
+//     fundRule?: QueryReturnType;
+//     instance?: QueryReturnType;
+//     thread?: QueryReturnType;
+//     comment?: QueryReturnType;
+//     tag?: QueryReturnType;
+//     bookmark?: QueryReturnType;
+//     wallet?: QueryReturnType;
+//     userSetting?: QueryReturnType;
+//     [key?: string]: QueryReturnType | undefined;
+//    };
+// }
+interface QueryInterface  { 
     spaces?: QueryReturnType;
     users?: QueryReturnType;
     proposals?: QueryReturnType;
@@ -45,4 +73,11 @@ interface RequestCustom extends Request {
     userSetting?: QueryReturnType;
     [key?: string]: QueryReturnType | undefined;
    };
-}
+
+   interface ParamsInterface { 
+    [key: string]: string
+    idMongoose?: string;
+    id?: string;
+    userId?: string
+   };
+
