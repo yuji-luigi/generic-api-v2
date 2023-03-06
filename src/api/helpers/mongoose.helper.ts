a570517d6ee8357b75c852015436e4f7c8f6eefd
Yuji Sato <--replace-all>
2023-03-06 10:27:03 +0100

    cloned query to raw query in aggregation. issue for objectId

--- src/api/helpers/mongoose.helper.ts
+++ src/api/helpers/mongoose.helper.ts
@@ -3,48 +3,52 @@ import mongoose from 'mongoose';
 // todo: aggregation method
 
 interface PaginatedResult {
-  paginatedResult: Array<any>
-}[];
+  paginatedResult: Array<any>;
+}
+[];
 interface Counts {
   counts: {
-    total: number
-  }[]
-}[];
+    total: number;
+  }[];
+}
+[];
 
-type ResultAggregateWithPagination = (PaginatedResult & Counts)
+type ResultAggregateWithPagination = PaginatedResult & Counts;
 
 /**
  *
  * @returns {[Document[],Counts]}
  */
-export async function aggregateWithPagination(query: any, entity: string): Promise<ResultAggregateWithPagination[]> {
+export async function aggregateWithPagination(
+  query: any,
+  entity: string
+): Promise<ResultAggregateWithPagination[]> {
   /** define skip value, then delete as follows */
   const limit = 10;
-  let skip = +query.skip -1 <= 0 ? 0 : (+query.skip - 1) * limit;
+  let skip = +query.skip - 1 <= 0 ? 0 : (+query.skip - 1) * limit;
   skip = isNaN(skip) ? 0 : skip;
-  delete query.skip;// not good way for functional programming. set new query object for querying the DB
+  delete query.skip; // not good way for functional programming. set new query object for querying the DB
   delete query.limit;
 
   // const clo nedQuery = structuredClone(query);
-  const clonedQuery = JSON.parse(JSON.stringify(query));
-
-  if(clonedQuery.parentId){
-    clonedQuery.parentId = new mongoose.Types.ObjectId(query.parentId);
+  // const clonedQuery = JSON.parse(JSON.stringify(query));
+  if (query.parentId) {
+    query.parentId = new mongoose.Types.ObjectId(query.parentId);
   }
-  const data = await mongoose.model(entity).aggregate<ResultAggregateWithPagination>([{
-    $facet: {
-      paginatedResult: [
-        {$match: clonedQuery },
-        {$skip: skip},
-        {$limit: limit}
-      ],
+  const data = await mongoose
+    .model(entity)
+    .aggregate<ResultAggregateWithPagination>([
+      {
+        $facet: {
+          paginatedResult: [
+            { $match: query },
+            { $skip: skip },
+            { $limit: limit }
+          ],
 
-      counts:[
-        {$match: clonedQuery},
-        {$count: 'total'}
-      ]
-    }
-
-  }]);
+          counts: [{ $match: query }, { $count: 'total' }]
+        }
+      }
+    ]);
   return data;
 }
