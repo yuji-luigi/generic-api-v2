9276d8a41bdecb4462ad21f606601cf46710af59
Yuji Sato <--replace-all>
2023-03-06 10:28:45 +0100

    handle query and body sequenced by setUser middleware

--- src/middlewares/handleQuery.ts
+++ src/middlewares/handleQuery.ts
@@ -1,29 +1,55 @@
+import { RequestCustom } from './../types/custom-express/express-custom.d';
 import httpStatus from 'http-status';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 
-interface RequestWithUser extends Request {
-  user: IUser}
+type RequestWithOwner = RequestCustom<
+  unknown,
+  unknown,
+  { owner: IOwner },
+  { owner: IOwner }
+>;
 
 export const handleQuery = (
-  req: RequestWithUser,
+  req: RequestWithOwner,
   res: Response,
   next: NextFunction
-):void => {
-
-  if(req.user.role === 'super_admin') {
+): void => {
+  if (req.user.role === 'super_admin') {
     return next();
   }
 
-  if(req.user.role !== 'super_admin') {
-    req.query = {
-      ...req.query,
-      owner: req.user.owner.toString()
-    };
+  if (req.user.role !== 'super_admin') {
+    req.query.owner = req.user.owner;
     return next();
   }
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+
+  return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: 'Something went wrong'
-  });
-  return null;
+  }) as never;
 };
+
+const setBody = (req: RequestWithOwner, res: Response, next: NextFunction) => {
+  const { user } = req;
+  if (req.user.role === 'super_admin') {
+    return next();
+  }
+
+  req.body.owner = user.owner;
+  return next();
+};
+
+export const handleOwner =
+  () =>
+  (req: RequestWithOwner, res: Response, next: NextFunction): void => {
+    if (!req.user) {
+      return next();
+    }
+    if (req.method === 'POST') {
+      return setBody(req, res, next);
+    }
+    if (req.method === 'GET') {
+      return handleQuery(req, res, next);
+    }
+    return next();
+  };
