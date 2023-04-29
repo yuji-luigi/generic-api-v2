5c1519c35f291ffe4cc73b8f6025464cea98114f
Yuji Sato <--replace-all>
2023-04-29 11:14:44 +0200

    msg func

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -48,3 +48,25 @@ const MSG = (arg?: argument) => ({
 
 export default MSG;
 // TODO: MAKE THEM INDIVUSUAL SO THAT SINGLE MSG WILL BE EXPORTED
+
+export const _MSG = {
+  OBJ_CREATED: 'Creazione oggetto avvenuto con successo',
+  OBJ_UPDATED: 'Aggiornamento oggetto avvenuto con successo',
+  OBJ_DELETED: 'Eliminazione oggetto avvenuto con successo',
+  OBJ_NOT_FOUND: 'Oggetto non trovato',
+  OBJ_NOT_DELETED: 'Oggetto non eliminato',
+  OBJ_NOT_UPDATED: 'Oggetto non aggiornato',
+  OBJ_NOT_CREATED: 'Oggetto non creato',
+  EMPLOYEE_CREATED: 'Creazione dipendente avvenuto con successo',
+  EMPLOYEE_UPDATED: 'Aggiornamento dipendente avvenuto con successo',
+  EMPLOYEE_DELETED: 'Eliminazione dipendente avvenuto con successo',
+  EMPLOYEE_NOT_FOUND: 'Dipendente non trovato',
+  USER_CREATED: 'Creazione utente avvenuto con successo',
+  USER_UPDATED: 'Aggiornamento utente avvenuto con successo',
+  USER_DELETED: 'Eliminazione utente avvenuto con successo',
+  USER_NOT_FOUND: 'Utente non trovato',
+  NOT_AUTHORIZED: "Non hai permesso per l'azione.",
+  NOT_FOUND_ID: (entity: string, id: string) => `Model ${entity} con id:${id} non trovato`,
+  MAIL_SEND_ERROR: "Errore durante l'invio della mail. Riprovo esecuzione del checkForExpiration la mail tra 1 minuto",
+  NOT_ALLOWED: 'you are not allowed to do this'
+};
