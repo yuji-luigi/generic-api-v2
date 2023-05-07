60ce50559e32fbfe7f9be8671cad6316289a0aec
Yuji Sato <--replace-all>
2023-05-07 19:13:30 +0200

    update: organization route - selected to cookie

--- src/api/routes/organization.routes.ts
+++ src/api/routes/organization.routes.ts
@@ -18,11 +18,11 @@ const router = express.Router();
 
 router.get('/', isLoggedIn(), sendOrganizations);
 router.get('/selections/super-admin', isLoggedIn([SUPER_ADMIN]), sendOrganizationsSelectionForSuperAdmin);
-router.get('/selected/:organizationId', isLoggedIn(), organizationSelected);
+router.get('/cookie/:organizationId', isLoggedIn(), organizationSelected);
 
 router.put('/:organizationId', isLoggedIn(), updateOrganizationById);
-
-router.delete('/selected', isLoggedIn(), deleteOrganizationCookie);
+// COOKIE
+router.delete('/cookie', isLoggedIn(), deleteOrganizationCookie);
 router.delete('/with-pagination/:organizationId', isLoggedIn([ADMIN, LOGGED_USER, SUPER_ADMIN]), deleteOrganizationByIdWithPagination);
 router.delete('/:organizationId', isLoggedIn([ADMIN, LOGGED_USER, SUPER_ADMIN]), deleteOrganizationByIdWithPagination);
 router.delete('/with-pagination/linkedChildren/:idMongoose', (req: Request, res: Response) => res.status(httpStatus.FORBIDDEN).send('forbidden'));
